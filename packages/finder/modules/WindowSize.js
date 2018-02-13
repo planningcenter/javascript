2d75f11b7d4a141b50850001b5bfce1b9eec1fd9
Michael Chan <mijoch@gmail.com>
2018-02-13 11:05:12 -0800

    finder: add working WindowSize code

--- packages/finder/modules/WindowSize.js
+++ packages/finder/modules/WindowSize.js
@@ -1,7 +1,34 @@
 import * as React from "react";
 
 export class WindowSize extends React.Component {
+  constructor() {
+    super();
+    this.state = { width: 0 };
+
+    this.setWidth = this.setWidth.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.setWidth);
+    this.setWidth();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.setWidth);
+  }
+
+  setWidth() {
+    this.setState({ width: window.innerWidth });
+  }
+
   render() {
-    return <div />
+    return this.props.render(this.state);
   }
 }
+WindowSize.propTypes = {
+  render: PropTypes.func.isRequired
+};
+
+WindowSize.defaultProps = {
+  render: () => <div>Left side!</div>
+};
