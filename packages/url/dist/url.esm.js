b38e071887ed573ca305900cc1831585001a4d02
Michael Chan <mijoch@gmail.com>
2018-04-26 13:24:00 -0700

    update deps and untrack dist/

--- packages/url/dist/url.esm.js
+++
@@ -1,2 +0,0 @@
-function n(n){return function(t){if(n&&t)switch(n){case"production":return"https://"+t+".planningcenteronline.com";case"staging":return"https://"+t+"-staging.planningcenteronline.com";case"development":case"test":return"http://"+t+".pco.test";default:return"http://"+t+".planningcenteronline.com"}}}export default n;
-//# sourceMappingURL=url.esm.js.map
