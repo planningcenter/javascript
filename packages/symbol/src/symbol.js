02108556a2eafac3c7242f3005796dc87d003e35
Michael Chan <mijoch@gmail.com>
2018-06-06 14:15:44 -0700

    fixes an issue where className was being overriden by incoming props because i'm a dummy

--- packages/symbol/src/symbol.js
+++ packages/symbol/src/symbol.js
@@ -1,14 +1,14 @@
 import { createElement } from "react";
 
-function Symbol({ symbol: _symbol, ...props }) {
+function Symbol({ className, symbol: _symbol, ...platformProps }) {
   let [path, symbol] = _symbol.replace(".svg", "").split("#");
 
   return (
     <svg
       role="img"
       title={`${symbol} icon`}
-      className={["symbol", props.className].join(" ").trim()}
-      {...props}
+      className={["symbol", className].join(" ").trim()}
+      {...platformProps}
     >
       <use xlinkHref={`${path}.svg#${symbol}`} />
     </svg>
