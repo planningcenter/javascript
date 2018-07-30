26def4d3f7ac94fb5f72cb579411d54b87f529dd
Michael Chan <mijoch@gmail.com>
2018-07-30 14:59:07 -0700

    WIP

---
+++ packages/doxy/tmp/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "default", {
+  enumerable: true,
+  get: function get() {
+    return _button.default;
+  }
+});
+
+var _react = _interopRequireDefault(require("react"));
+
+var _button = _interopRequireDefault(require("./button.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
